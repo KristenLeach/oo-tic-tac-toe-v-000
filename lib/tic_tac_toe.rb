d6bf537c19afcfe3ce6af5c8642a0d15b54e7eee
Kristen Leach <kristenleach24@gmail.com>
2017-11-30 00:26:59 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,13 +1,6 @@
 class TicTacToe
   def initialize(board)
-    @board = board[]
-  end
-  def board=(board)
-    @board = board
-  end
-  def board
-    @board
-  end
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
   
   WIN_COMBINATIONS = [
   [0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]
