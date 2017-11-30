264c44686369abccae5a8ef0a419fa6f47635a02
Kristen Leach <kristenleach24@gmail.com>
2017-11-30 02:14:50 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -77,14 +77,14 @@ def current_player
     end
 end
 
- def won?(board)
+ def won?
     WIN_COMBINATIONS.any? do |combo|
-          if board[combo[0]] == "X" && 
-          board[combo[1]] == "X" && 
-          board[combo[2]] == "X" || 
-          board[combo[0]] == "O" && 
-          board[combo[1]] == "O" && 
-          board[combo[2]] == "O"
+          if @board[combo[0]] == "X" && 
+          @board[combo[1]] == "X" && 
+          @board[combo[2]] == "X" || 
+          @board[combo[0]] == "O" && 
+          @board[combo[1]] == "O" && 
+          @board[combo[2]] == "O"
           return combo
           end
       end
